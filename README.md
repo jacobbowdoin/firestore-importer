f76402877389e366049296aa88c8c78ec7bcd8f8
Jacob Bowdoin <jwbowdoin@gmail.com>
2018-11-16 17:49:12 -0800

    Ability to check unique POS and import data+audio

--- README.md
+++ README.md
@@ -4,10 +4,13 @@
 Built with help from https://angularfirebase.com/lessons/import-csv-json-or-excel-to-firestore/
 
 ### To Compile Typescript into Javascript after updates
-Run `tsc`
-(must have installed Typescript beforehand)
+Run `tsc` (must have installed Typescript beforehand)
+or press `Ctrl+Shift+B` and select `tsc: watch - tsconfig.json` to start the Typescript watcher
 
-### Example of how to run script
-Run `firestore-import --src file.json --collection dictionaries/id/words --environment prod`
-Run `firestore-import -s v1-data/chamacoco_export.json --collection dictionaries/id/words --environment prod`
+### Examples of how to run script
+Run `firestore-import -s <source.json> -d <dictionaryId> -n <dictionaryName> --environment prod`
+For example, to import Chamococo to the dev site, run `firestore-import -s v1-data/chamacoco_export.json -d yRl8SvrwmeyckpCHU1X5 -n Chamococo`
+
 It defaults to dev environment if prod not mentioned (environment is an optional argument). See https://github.com/tj/commander.js/ for help with required and optional arguments.
+
+// TODO: add audio location argument as presently it must be manually written into .ts file
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import * as args from 'commander';
 import * as admin from 'firebase-admin';
 import * as fs from 'fs-extra';
 import { join } from 'path';
+const UUID = require('uuidv4');
 
 args
     .version("0.0.1")
     .option("-s, --src <path>", "Source file path")
-    .option("-c, --collection <path>", "Collection path in firestore")
+    // .option("-c, --collection <path>", "Collection path in firestore")
+    .option("-d, --dictionaryId <dictionaryId>", "Dictionary Id in firestore")
+    .option("-n, --dictionaryName <dictionaryName>", "Dictionary name, used in saving media files")
     .option("-e, --environment [dev/prod]", "Firebase Project")
     .parse(process.argv);
 
@@ -23,51 +26,58 @@ const db = admin.firestore();
 
 async function importToFirestore() {
     try {
-        // TODO: figure out collection path and test a few data uploads to firestore (5)
-        // const colPath = args.collection;
+        const colPath = `dictionaries/${args.dictionaryId}/words`;
         const file = args.src;
 
-        // Create a batch to run an atomic write
-        // const colRef = db.collection(colPath);
-        // const batch = db.batch();
+        const colRef = db.collection(colPath);
+        const batch = db.batch();
 
         let data;
         if (file.includes(".json")) {
             data = await fs.readJSON(file);
         }
 
+        const commitRound = 0; // start at 0
+        const batchStart = 0 + (500 * commitRound);
+        const batchEnd = 499 + (500 * commitRound);
+
         for (let i = 0; i < data.length; i++) {
-            // console.log(data[i]);
-            data[i].lx = data[i].lang;
+            // Firestore 'cannot write more than 500 entities in a single call' so we have to upload in chunks
+            // See https://github.com/firebase/firebase-admin-java/issues/106 for a possible automated chunking solution
+            if ((i < batchStart) || (i > batchEnd)) { continue; };
+
+            data[i].lx = data[i].lang || '';
             delete data[i].lang
 
-            data[i].ph = data[i].ipa;
+            data[i].ph = data[i].ipa || '';
             delete data[i].ipa;
 
-            data[i].ps = data[i].pos;
+            // TODO: convert old parts of speech to new abbreviations
+            data[i].ps = data[i].pos || '';
             delete data[i].pos;
 
-            data[i].di = data[i].dialect;
+            data[i].di = data[i].dialect || '';
             delete data[i].dialect;
 
-            data[i].xv = data[i].usage_example;
+            data[i].xv = data[i].usage_example || '';
             delete data[i].usage_example;
 
-            data[i].lc = data[i].metadata; // location
+            data[i].lc = data[i].metadata || ''; // location
             delete data[i].metadata;
 
             // learn about try/catch so I can convert this to const uploadedAudioPath = await upload...()
             // maybe the outer parent catch will even catch this? Test it out.
-            await uploadAudioFile(data[i].audio, data[i].lx)
-                .then((uploadedAudioPath) => {
-                    console.log('audio saved as ', uploadedAudioPath);
 
-                    const dateArray = data[i].audio.match(/(\d*)_(\d*)_(\d*)/);
+            const entryId = colRef.doc().id;
+
+            await uploadAudioFile(data[i].audio, data[i].lx, entryId)
+                .then((response: any) => {
+                    const dateArray = data[i].audio.match(/([0-9]*)_([0-9]*)_([0-9]*)/);
                     data[i].sf = {
-                        cr: data[i].authority, // speaker
+                        cr: data[i].authority || '', // speaker
                         ts: dateArray ? new Date(`${dateArray[1]}, ${dateArray[2]}, ${dateArray[3]}`) : null,
-                        fp: uploadedAudioPath,
-                        // mt: 'TODO: get media token returned',
+                        path: response.uploadedAudioPath,
+                        mt: response.uuid,
                     };
                     delete data[i].authority;
                     delete data[i].audio;
@@ -76,29 +86,22 @@ async function importToFirestore() {
 
             delete data[i].image;
 
-            data[i].sd = data[i].semantic_ids;
+            data[i].sd = data[i].semantic_ids || '';
             delete data[i].semantic_ids;
 
-            data[i].gloss = {
-                English: data[i].gloss,
-                Español: data[i].es_gloss,
+            data[i].gl = {
+                English: data[i].gloss || '',
+                Español: data[i].es_gloss || '',
             }
+            delete data[i].gloss;
             delete data[i].es_gloss;
 
-            // console.log(data[i]);
-            // break;
+            const docRef = colRef.doc(entryId);
+            batch.set(docRef, data[i]);
+            console.log(`Added ${i} to batch: ${data[i].lx}`);
         }
 
-        //TODO work batch uploading into previous for loop
-        // for (const item of data) {
-        //     const id = args.id ? item[args.id].toString() : colRef.doc().id;
-
-        //     const docRef = colRef.doc(id);
-
-        //     batch.set(docRef, item);
-        // }
-
-        // await batch.commit();
+        await batch.commit();
         console.log("Firestore import completed successfully.");
     } catch (error) {
         console.log("Migration failed!", error);
@@ -108,29 +111,56 @@ async function importToFirestore() {
 import * as Storage from '@google-cloud/storage';
 const gcs = new Storage();
 
-const uploadAudioFile = (audioFileName, lexeme) => {
+const uploadAudioFile = (audioFileName, lexeme, entryId) => {
     return new Promise((resolve, reject) => {
         if (!audioFileName) { reject(`No audio found for ${lexeme}`) };
 
+        const fileBucket = `talking-dictionaries-${args.environment == 'prod' ? 'alpha' : 'dev'}.appspot.com`
+
+        // TODO: add audio location argument or manually write
         const audioDir = join(__dirname, '../v1-audio');
         const audioFilePath = join(audioDir, audioFileName);
 
-        const dictionaryName = 'Chamococo';
-        const dictionaryId = '123';
         const uploadedAudioName = lexeme.replace(/ /g, '_').replace(/\./g, '');
-        const uploadedAudioPath = `audio/${dictionaryName}_${dictionaryId}_${uploadedAudioName}_123`;
+        const audioType = audioFileName.match(/\.[0-9a-z]+$/i);
 
-        const fileBucket = `talking-dictionaries-${args.environment == 'prod' ? 'alpha' : 'dev'}.appspot.com/`
+        const uploadedAudioPath = `audio/${args.dictionaryName}_${args.dictionaryId}/${uploadedAudioName}_${entryId}${audioType}`;
 
-        // TODO: Try uploading image to storage
+        let uuid = UUID();
+
         gcs.bucket(fileBucket).upload(audioFilePath, {
-            destination: join(bucketDir, uploadedAudioPath)
-        }).then(() => { resolve(uploadedAudioPath) })
+            destination: uploadedAudioPath,
+            metadata: {
+                metadata: {
+                    firebaseStorageDownloadTokens: uuid
+                }
+            }
+        }).then(() => {
+            resolve({ uploadedAudioPath, uuid });
+        })
             .catch((err) => { reject(err) });
     })
 }
 
 importToFirestore();
+
+async function gatherPartsOfSpeech() {
+    const file = args.src;
+    let data;
+    if (file.includes(".json")) {
+        data = await fs.readJSON(file);
+    }
+
+    let partsOfSpeech = [];
+    for (let i = 0; i < data.length; i++) {
+        const pos = data[i].pos;
+
+        if (partsOfSpeech.indexOf(pos) === -1) partsOfSpeech.push(pos);
+    }
+    console.log(partsOfSpeech);
+}
+
+// gatherPartsOfSpeech();
 
 // Audio currently stored as
 // audio/Bahasa_Lani_jaRhn6MAZim4Blvr1iEv/eke_z8830ipzn7vgjbbx_1541390364420.mp3
@@ -143,3 +173,19 @@ importToFirestore();
 //     console.log(files.map(f => '\t' + f).join('\n'));
 //     console.log(`Contents of ${join(__dirname, '../v1-audio')}`);
 // });
+
+
+// Media Token Retrieval
+// Learned from https://stackoverflow.com/questions/42956250/get-download-url-from-file-uploaded-with-cloud-functions-for-firebase 
+// on how to generate media token for sound file.
+// Could have also used Laurent's answer: https://stackoverflow.com/a/49411199/7053575
+// gcs.bucket(fileBucket).upload(audioFilePath, {
+//     destination: uploadedAudioPath,
+//     predefinedAcl: 'publicRead'
+// }).then((result) => {
+//     const file = result[0];
+//     return file.getMetadata();
+// }).then(results => {
+//     const metadata = results[0];
+//     resolve(metadata.mediaLink);
+// })
